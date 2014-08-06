5c4cc904876e0415f080553cee0d43593384cec3
John Jarvis <john@jarv.org>
2014-08-06 16:15:21 -0500

    Update documentation

--- game.js
+++ game.js
@@ -642,7 +642,7 @@ var game = {
 
     function updateHS() {
       $.ajax({
-          url: 'hs.stategame.jarv.org/d',
+          url: 'http://hs.stategame.jarv.org/d',
           cache: 'false',
           dataType: 'json',
           type: 'get',
@@ -680,7 +680,7 @@ var game = {
           $('#input').prop("disabled", true);
           n = $('#input').val();
           $.ajax({
-                  url: 'hs.stategame.jarv.org/d',
+                  url: 'http://hs.stategame.jarv.org/d',
                   cache: 'false',
                   dataType: 'json',
                   type: 'post',
